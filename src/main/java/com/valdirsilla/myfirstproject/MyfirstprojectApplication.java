934b545d9cb221fc5732591f8ba4a4f20533af50
Valdir Silva <valdirpiresba@gmail.com>
2021-10-09 19:11:09 -0300

    Product memory

--- src/main/java/com/valdirsilla/myfirstproject/MyfirstprojectApplication.java
+++ src/main/java/com/valdirsilla/myfirstproject/MyfirstprojectApplication.java
@@ -1,18 +1,25 @@
 package com.valdirsilla.myfirstproject;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.valdirsilla.myfirstproject.entities.Category;
+import com.valdirsilla.myfirstproject.entities.Product;
 import com.valdirsilla.myfirstproject.repositories.CategoryRepository;
+import com.valdirsilla.myfirstproject.repositories.ProductRepository;
 
 @SpringBootApplication
 public class MyfirstprojectApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository  productRepository; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstprojectApplication.class, args);
@@ -24,9 +31,23 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 		
 		Category cat1 = new Category(1L, "Electronics");
 		Category cat2 = new Category(2L, "Books");
+
+		Product p1 = new Product(1L, "TV", 2200.00, cat1);
+		Product p2 = new Product(2L, "Domain Driven Design", 120.00, cat2);
+		Product p3 = new Product(3L, "PS5", 2800.00, cat1);
+		Product p4 = new Product(4L, "Docker", 100.00, cat2);
+
+		cat1.getProducts().addAll(Arrays.asList(p1, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2, p4));
+
 		
         categoryRepository.save(cat1);
         categoryRepository.save(cat2);
+        
+        productRepository.save(p1);
+        productRepository.save(p2);
+        productRepository.save(p3);
+        productRepository.save(p4);
 		
 	}
 
--- src/main/java/com/valdirsilla/myfirstproject/entities/Product.java
+++ src/main/java/com/valdirsilla/myfirstproject/entities/Product.java
@@ -1,34 +1,29 @@
 package com.valdirsilla.myfirstproject.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public class Category implements Serializable {
+public class Product implements Serializable {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
+	private Double price;
+	    
+	private Category category;
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>();
-	
-	
-	public Category() {
+	public Product() {
 		
 	}
 	
-	
-	public Category(Long id, String name) {
+	public Product(Long id, String name, Double price, Category category) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -47,15 +42,29 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 	
 
-	public List<Product> getProducts() {
-		return products;
-	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -65,8 +74,10 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
+	
+	
 
 }
